d0f192bbc7e87c30b91b3a0603e89064fef28992
2dd <79iug12@gmail.com>
2019-11-29 23:53:04 +0900

    go coinfit

--- server/index.js
+++ server/index.js
@@ -6,16 +6,6 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-if(process.env.NODE_ENV === 'production'){
-    const path = require('path');
-    app.use(express.static(path.join('client/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client', 'build','index.html'));
-    });
-    // const staticFiles = express.static(path.join(__dirname, '../../client/build'));
-    // app.use(staticFiles);
-    // app.get('/*', staticFiles)
-}
 
 const port = process.env.PORT || 5000;
 app.use(cors());
@@ -41,6 +31,18 @@ const usersRouter = require('./routes/users');
 //app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use('/api/exercises', exercisesRouter);
+
+if(process.env.NODE_ENV === 'production'){
+    const path = require('path');
+    app.use(express.static(path.join('client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client', 'build','index.html'));
+    });
+    // const staticFiles = express.static(path.join(__dirname, '../../client/build'));
+    // app.use(staticFiles);
+    // app.get('/*', staticFiles)
+}
+
 
 app.listen(port, ()=>{
     console.log(`Server is running on port: ${port}`)
